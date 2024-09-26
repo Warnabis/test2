729bb368f0e6ef5ef310b6dc035cf49204e3b764
Trifonko V. <vtrifonko@gmail.com>
2024-09-26 20:13:37 +0300

    Update subscription.h

--- src/subscription.h
+++ src/subscription.h
@@ -2,26 +2,23 @@
 #define SUBSCRIPTION_H
 
 #include <iostream>
-#include <string>
-#include <vector>
 
 class Subscription {
 public:
     int id;
     std::string name;
-    int price;
+    double price;
     int days;
 
+    Subscription();
     void input();
     void output() const;
-    void create(std::vector<Subscription>& services) const;
-    void read(const std::vector<Subscription>& services) const;
-    void update(std::vector<Subscription>& services) const;
-    void deletes(std::vector<Subscription>& services) const;
-    void workout(std::vector<Subscription>& services, Subscription*& selectedservice) const;
-
-private:
-    void wait() const;
+    void create(Subscription*& services, int& size);
+    void read(const Subscription* services, int size) const;
+    void update(Subscription* services, int size);
+    void deletes(Subscription*& services, int& size);
+    void workout(Subscription* services, int size, Subscription*& selectedservice) const;
 };
 
-#endif
+void wait();
+void choose(int choice, Subscription& service, Subscription*& services, int& size, Subscription*& selectedservice);
